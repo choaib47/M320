4f2605bff95a001cff92c39ae0662f5b165b2308
Choaib Bourahla <Choaib.Bourahla@ksb-sg.ch>
2024-08-26 11:47:07 +0200

    Auftrag Instanzvariabeln

---
+++ typeshi/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace typeshi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle smallRectangle = new Rectangle();
+            Rectangle bigRectangle = new Rectangle();
+            Rectangle verybigRectangle = new Rectangle();
+
+            Console.WriteLine(smallRectangle.width);
+            
+        }
+    }
+}
---
+++ typeshi/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace typeshi
+{
+    internal class Rectangle
+    {
+        //1. implemntierung feld 
+        private int height;
+
+        //2. feld dessen wert nicht verändert
+        private readonly int width = 5;
+
+        //3. 
+        private int area = 0;
+
+
+    }
+}
